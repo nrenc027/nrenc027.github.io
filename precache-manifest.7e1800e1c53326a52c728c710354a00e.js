8e548f35ff182d8c521f221265a11c4de432a857
Ren Estep <n.ren.cummings@gmail.com>
2019-08-31 20:08:09 -0400

    Updates

---
+++ precache-manifest.7e1800e1c53326a52c728c710354a00e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9367ee86d3cb3882e54d5b281c9f2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37360f7589a056324859",
+    "url": "/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "fbbb49655fb0b25b6f7c",
+    "url": "/static/js/2.11f36b89.chunk.js"
+  },
+  {
+    "revision": "37360f7589a056324859",
+    "url": "/static/js/main.7c32ba7f.chunk.js"
+  },
+  {
+    "revision": "39f27cb349b42b47f208",
+    "url": "/static/js/runtime~main.ec6d71d4.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
